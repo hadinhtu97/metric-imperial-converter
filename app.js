2013265c3bad989c9f6456e576273345d7fa551e
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-16 14:47:53 +0700

    perpare for test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 app.use(express.static(__dirname + '/public'));
 
-const listMetricImperial = [
+const listConvert = [
     ['gal', 'L', 3.78541],
     ['lbs', 'kg', 0.453592],
     ['mi', 'km', 1.60934],
@@ -28,29 +28,54 @@ const isUnit = (unit) => {
     return false;
 }
 
+const convert = (num, unit) => {
+    for (let i = 0; i < listConvert.length; i++) {
+        if (listConvert[i].indexOf(unit) != -1) {
+            if (listConvert[i].indexOf(unit) == 0) return [num * listConvert[i][2], listConvert[i][1]];
+            if (listConvert[i].indexOf(unit) == 1) return [num / listConvert[i][2], listConvert[i][0]];
+        }
+    }
+}
+
 app.route('/api/convert').get((req, res) => {
     if (req.query.input === undefined) {
-        res.json("Can't not find 'input' value!");
+        res.send("Can't not find 'input' value!");
     } else {
+
         let input = req.query.input;
         let regex = /[^\d.\W]/;
         let firstLetterOfUnit = input.match(regex);
+
         if (firstLetterOfUnit === null) {
             if (isNaN(input)) {
-                res.json('invalid number and unit')
+                res.send('invalid number and unit')
+            } else {
+                res.send('invalid unit')
             }
-            res.json('invalid unit')
-        }
-        let num = input.slice(0, firstLetterOfUnit.index);
-        let unit = input.slice(firstLetterOfUnit.index);
-        if (!isUnit(unit) && isNaN(num)) {
-            res.json('invalid number and unit')
-        }
-        if (isNaN(num)) {
-            res.json('invalid number')
-        }
-        if (!isUnit(unit)) {
-            res.json('invalid unit')
+        } else {
+            let num = input.slice(0, firstLetterOfUnit.index);
+            let unit = input.slice(firstLetterOfUnit.index);
+            if (num == '') num = 1;
+            num = eval(num);
+            if (!isUnit(unit) && isNaN(num)) {
+                res.send('invalid number and unit')
+            } else if (isNaN(num)) {
+                res.send('invalid number')
+            } else if (!isUnit(unit)) {
+                res.send('invalid unit')
+            } else {
+                unit = unit.toLowerCase();
+                if (unit == 'l') unit = 'L';
+                let returnNum = parseFloat(convert(num, unit)[0].toFixed(5));
+                let returnUnit = convert(num, unit)[1];
+                res.json({
+                    initNum: num,
+                    initUnit: unit,
+                    returnNum: returnNum,
+                    returnUnit: returnUnit,
+                    string: num + ' ' + unit + ' converts to ' + returnNum + ' ' + returnUnit
+                })
+            }
         }
     }
 })
