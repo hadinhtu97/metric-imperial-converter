b78798f2d8e1433cb14e79607441d957f79d2224
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-16 19:52:07 +0700

    update frontend

---
+++ public/client.js
@@ -0,0 +1,9 @@
+let form = document.getElementById('form');
+let input = document.getElementById('input');
+let output = document.getElementById('output');
+let string = document.getElementById('string');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+})
